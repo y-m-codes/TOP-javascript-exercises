40ab7b7b5e0f870f240d9e080d1ea4a585b2910c
Yogita Maruti <157566073+y-m-codes@users.noreply.github.com>
2026-04-27 20:24:56 +1000

    Completed arithmetic functions.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0
+  }
+  else if (arr !== undefined || arr.length !== 0) {
+    return arr.reduce((accumulator, currentValue) => accumulator + currentValue)
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator * currentValue)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+// const factorialArray = function(number) {
+//   let numbersArray = [];
+
+//   for (let i = 1; i < number + 1; i++) {
+//     numbersArray.push(i);
+//   };
+
+//   let numberFactorial = numbersArray.reduce((accumulator, currentValue) => accumulator * currentValue);
+
+//   return numberFactorial;
+// };
+
+const factorial = function(number) {
+  let accumulator = 1;
+  for (let i = number; i > 0; i--) {
+    accumulator *= i;
+  };
+  return accumulator;
 };
 
 // Do not edit below this line
