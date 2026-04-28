54304ac8338782da480674fec63b834231b16f54
Yogita Maruti <157566073+y-m-codes@users.noreply.github.com>
2026-04-28 17:12:56 +1000

    Completed palindromes exercise.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
+const palindromes = function(string) {
+  let stringLowerCase = string.toLowerCase();
+  let stringCleaned = stringLowerCase.replace(/[^a-zA-Z]/g, "");
 
+  function reverseString(string) {
+    let reversedString = "";
+    for (i = string.length - 1; i >= 0; i--) {
+        reversedString += string[i];
+    }
+    return reversedString
+  };
+
+  let stringInReverse = reverseString(stringCleaned);
+
+  if (stringCleaned === stringInReverse) {
+    return "This is a palindrome."
+  } else {
+    return "This is NOT a palindrome."
+  };
 };
 
+const testString = "A car, a man, a maraca";
+
+console.log(palindromes(testString));
+console.log(palindromes("Rats live on no evil star."));
+console.log(palindromes("Lid off a daffodil."));
+console.log(palindromes("Animal loots foliated detail of stool lamina."));
+console.log(palindromes("A nut for a jar of tuna."));
+
+
 // Do not edit below this line
-module.exports = palindromes;
+// module.exports = palindromes;
